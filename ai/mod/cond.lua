7a17a6ccb17addf62522a18891c9c8d1cd15de14
OhnkytaBlabdey <1263189143@qq.com>
2017-08-20 11:01:26 +0800

    Add files via upload
    可能批量加入combo

---
+++ ai/mod/cond.lua
@@ -0,0 +1,18 @@
+---cond.lua
+function conditioncheck()
+--做完一步后，判断是否满足进入下一步的条件，返回T/F
+ 
+--ver test 
+if 0 == 0 then 
+return true
+end
+
+---
+--
+if ob1 == 0 and ob2 == 0 then 
+  return true
+end
+
+
+
+end 
---
+++ ai/mod/object.lua
@@ -0,0 +1,35 @@
+--object.lua
+-- COMMAND_LET_AI_DECIDE  = -1
+-- COMMAND_SUMMON         = 0
+-- COMMAND_SPECIAL_SUMMON = 1
+-- COMMAND_CHANGE_POS     = 2
+-- COMMAND_SET_MONSTER    = 3
+-- COMMAND_SET_ST         = 4
+-- COMMAND_ACTIVATE       = 5
+-- COMMAND_TO_NEXT_PHASE  = 6
+-- COMMAND_TO_END_PHASE   = 7
+
+function obob()
+
+ if cpindi(1,1) then
+ command=5
+ ida=23332333
+ 
+ end
+ ----
+ 
+ 
+ 
+
+end
+ -- command=obcom()--command
+ -- gid=xxxxx
+ -- ida=oba()
+ -- idb=obb()
+ -- idc=obc()
+ -- seq=obseq()--seq 
+ ------------------xxxx
+ --- command,gid,ida,idb,idc,seq=obcal()
+-- end
+-- function obcal()
+ -- return command,gid,ida,idb,idc,seq
